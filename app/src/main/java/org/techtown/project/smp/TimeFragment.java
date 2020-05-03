20b28bc5f825c72512567bcdbcbecca50044f536
임수현 <wgtngus2@naver.com>
2020-05-03 17:05:49 +0900

    Updated:: SMP Fragment

---
+++ app/src/main/java/org/techtown/project/smp/TimeFragment.java
@@ -0,0 +1,20 @@
+package org.techtown.project.smp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.techtown.project.R;
+
+public class TimeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.example2, container, false);
+    }
+}
